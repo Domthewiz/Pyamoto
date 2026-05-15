991b2f8f65e5742dfdc8fefb7a27812284ee3747
halcyon-v <242117491+halcyon-v@users.noreply.github.com>
2026-05-14 21:58:52 -0400

    Metadata

--- spritelib.py
+++ spritelib.py
@@ -1,25 +1,11 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-# Miyamoto! Level Editor - New Super Mario Bros. U Level Editor
-# Copyright (C) 2009-2021 Treeki, Tempus, angelsl, JasonP27, Kinnay,
-# MalStar1000, RoadrunnerWMC, MrRean, Grop, AboodXD, Gota7, John10v10,
-# mrbengtsson
+# Pyamoto Level Editor
+# Copyright (C) 2009-2026 Pyamoto contributors
+# This file is part of Pyamoto.
 
-# This file is part of Miyamoto!.
-
-# Miyamoto! is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-
-# Miyamoto! is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-
-# You should have received a copy of the GNU General Public License
-# along with Miyamoto!.  If not, see <http://www.gnu.org/licenses/>.
+# See LICENSE.txt for more information.
 
 # spritelib.py
 # Contains general code to render sprite images
